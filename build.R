adc652c6101bda9fd19edf1841de6aff79c5da10
Jeremy Thoms Hetzel <jthetzel@gmail.com>
2011-04-06 00:00:00 +0000

    version 1.0-0

---
+++ build.R
@@ -0,0 +1,9 @@
+
+
+
+cat('"C:/Program Files/R/R-2.12.2/bin/x64/R" CMD build "C:/Users/jthetzel/Research/trapezoid/"')
+build <- '"C:/Program Files/R/R-2.12.2/bin/x64/R" CMD build "C:/Users/jthetzel/Research/trapezoid/" --binary'
+check <- '"C:/Program Files/R/R-2.12.2/bin/x64/R" CMD check "C:/Users/jthetzel/Research/trapezoid/"'
+system(build)
+
+
